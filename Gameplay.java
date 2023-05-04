cc6e60071f0ae33f0c97f653fd8d0bff4d2698f4
Dakota Law <dlaw14@smith.edu>
2023-05-03 20:47:13 -0400

    boo

--- Gameplay.java
+++ Gameplay.java
@@ -9,10 +9,10 @@ import java.util.*;
  */
 public class Gameplay {
     String avatarName;  
-    Double health; 
+    int health; 
     
 
-    public Gameplay(String avatarName, Double health){
+    public Gameplay(String avatarName, int health){
         this.avatarName = avatarName; 
         this.health = health; 
     }
@@ -23,7 +23,6 @@ public class Gameplay {
         Scanner intro = new Scanner(System.in); 
         System.out.println("Enter yes or no"); 
         String choiceOne = intro.nextLine(); 
-        intro.close(); 
         try{
             if(choiceOne == "yes"){
                 System.out.println("Great! What's your name?"); 
@@ -36,6 +35,7 @@ public class Gameplay {
         catch (Exception e){
             System.out.println("Something went wrong! Please either say yes or no to this question or this narrator will get angry");
         }
+        intro.close(); 
         
     }
 
@@ -44,15 +44,14 @@ public class Gameplay {
         System.out.println("Now you must chose who you want to go see first. Be careful though... there are dangers hissden around every corner!");
         System.out.println("Who will you decide to go see ... The Blacksmith, The Carpenter, and The Psychic. You can go see all of them, none of them, just one of them, or just two of them."); 
         System.out.println("Be careful though..."); 
-        Scanner pathOneChoiceBlacksmith = new Scanner(System.in); 
+        Scanner choiceOne = new Scanner(System.in); 
         System.out.println("Do you want to see The Blacksmith Answer yes or no"); 
-        String blacksmithChoice = pathOneChoiceBlacksmith.nextLine(); 
-        pathOneChoiceBlacksmith.close(); 
+        String blacksmith = choiceOne.nextLine();  
         try{
-            if(blacksmithChoice.equals("yes")) {
+            if(blacksmith.equals("yes")) {
                 System.out.println("Amazing choice! You must now journey to the beach town where you will meet the blacksmith."); 
             }
-            if (blacksmithChoice.equals("no")){
+            if (blacksmith.equals("no")){
                 System.out.println("Well I hope you are sure..."); 
             }
         }
@@ -67,7 +66,8 @@ public class Gameplay {
     }
 
     public static void main(String args[]) {
-        Gameplay Raley = new Gameplay("Raley"); 
+        Gameplay Raley = new Gameplay("Raley", 3); 
+        Raley.introduction();
 
     }
        
