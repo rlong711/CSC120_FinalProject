5314f1413f2a9a83f7345ccdea44d5097f5d6558
rlong711 <you@example.com>
2023-05-09 15:00:59 -0400

    delete files, etc.

--- Avatar.java
+++ Avatar.java
@@ -29,7 +29,7 @@ public class Avatar{
     }
 
     public ArrayList<String> getInventory(){
-        System.out.println();
+        System.out.println(inventory);
         return inventory;
     }
 
--- Gameplay.java
+++ Gameplay.java
@@ -4,6 +4,14 @@ import java.util.*;
  * Runs the actual game. 
  */
 public class Gameplay {
+
+    public static void main(String[] args) {
+       
+
+
+
+
+    }
     
 
        
